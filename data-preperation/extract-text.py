cd9f574d8120c893074ff94b557cc812bc9630a4
Christian Cham <christianfintacham@gmail.com>
2024-10-08 21:18:20 +0800

    change extraction to accomodate landscape pages

--- data-preperation/extract-text.py
+++ data-preperation/extract-text.py
@@ -5,36 +5,79 @@ import os
 
 
 # Directory paths
-input_dir = 'datasets/raw-datasets'
-output_dir = 'datasets/text-extracted'
+input_dir = 'data-preperation/datasets/raw-datasets'
+output_dir = 'data-preperation/datasets/text-extracted'
 
 
 # Make sure dir exists
 os.makedirs(output_dir, exist_ok = True)
 
+# Separate the landscape pdfs into two sections because pdfplumber by default 
+# reads from left to right, disregarding 
+def extract_text_landscape(pdf):
+    extracted_text = ''
 
-for filename in os.listdir(input_dir):
-    if filename.endswith('.pdf'):
-        pdf_path = os.path.join(input_dir, filename)
-        extracted_text = ''
+    for page in pdf.pages:
 
-        try: 
-            # Open PDF file and extract text
-            with pdfplumber.open(pdf_path) as pdf:
-                for page in pdf.pages:
-                    extracted_text += page.extract_text() or ''
+        # Ensure is landscape
+        if page.width > page.height:
+            # Separate into two sections
+            left_section = (0, 0, page.width / 2 , page.height)
+            right_box = (page.width / 2, 0, page.width, page.height)
+
+            # Extract text from each section
+            left_text = page.within_bbox(left_section).extract_text() or ''
+            right_text = page.within_bbox(right_box).extract_text() or ''
+
+            extracted_text += left_text + '\n\n' + right_text + '\n\n'
+        else:
+            extracted_text += page.extract_text() or ''
+    
+    return extracted_text
 
 
-            # Set the output filename
-            output_filename = f"{os.path.splitext(filename)[0]}_extracted.txt"
-            output_path = os.path.join(output_dir, output_filename)
+# Extract text
+def extract_text (input_dir, output_dir):
 
-            
-            # Store extract text in (.txt file)
-            with open(output_path, 'w', encoding='utf-8') as f:
-                f.write(extracted_text)
+    for filename in os.listdir(input_dir):
 
-        except Exception as e:
-            print(f"Error procesing {filename}: {e}")
+        if filename.endswith('.pdf'):
+            pdf_path = os.path.join(input_dir, filename)
+            extracted_text = ''
 
-print("Text extraction complete")
+            try: 
+                # Open PDF file 
+                with pdfplumber.open(pdf_path) as pdf:
+
+                    # Check orientation of second page (first page / cover page might be different)
+                    if len(pdf.pages) > 1:
+                        random_page = pdf.pages[1]  
+                        is_landscape = random_page.width > random_page.height
+                    else: 
+                        page = pdf.pages[0]
+                        is_landscape = page.width > page.height
+
+                    # Extract data based on portrait or landscape
+                    if is_landscape:
+                        extracted_text = extract_text_landscape(pdf)
+                    else: 
+                        # Open PDF file and extract text
+                        for page in pdf.pages:
+                            extracted_text += page.extract_text() or ''
+
+                    # Set the output filename
+                    output_filename = f"{os.path.splitext(filename)[0]}_extracted.txt"
+                    output_path = os.path.join(output_dir, output_filename)
+                    
+                    # Store extract text in (.txt file)
+                    with open(output_path, 'w', encoding='utf-8') as f:
+                        f.write(extracted_text)
+
+                    print(f"Succesfully processed {filename}")
+
+            except Exception as e:
+                print(f"Error procesing {filename}: {e}")
+
+
+extract_text(input_dir, output_dir)
+print("Done extraction")
